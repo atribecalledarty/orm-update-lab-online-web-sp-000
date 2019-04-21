b9e82066c68a44debb9544d3c1861d9ea2d88a7b
Arthur Song <arth3rs0ng@gmail.com>
2019-04-21 22:07:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,4 +48,17 @@ class Student
   def self.new_from_db (row)
     student = Student.new(row[0], row[1], row[2])
   end
+  
+  def self.find_by_name (name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
