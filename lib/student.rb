9323500d612058dc932322eba5e125ba80bbe3c3
Arthur Song <arth3rs0ng@gmail.com>
2019-04-21 21:58:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,5 +12,16 @@ class Student
     @name = name
     @grade = grade
   end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE studnets (
+        id INTEGER PRIMARY KEY
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute()
+  end
 
 end
